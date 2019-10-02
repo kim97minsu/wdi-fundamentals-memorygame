16b32269bccf92144b3c813e18ac761d14d51113
Minsu Kim <minsu@Minsus-MacBook-Pro.local>
2019-10-02 15:05:13 -0700

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,23 +1,26 @@
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
 
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+};
 
+function flipCard(cardId) {
+	if (cardsInPlay.length === 2) { 
+		if (cardsInPlay[0] === cardsInPlay[1]) { 
+			alert("You found a match!");
+		} else {
+			alert("Sorry, it doesn't match");
+		}
+	} 
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId]);
+	checkForMatch();
+};
 
-var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-
-
-
-cardsInPlay.push(cardOne);
-console.log("User flipped", cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped", cardTwo);
-
-if (cardsInPlay.length === 2) { 
-	if (cardsInPlay[0] === cardsInPlay[1]) { 
-		alert("You found a match!");
-	} else {
-		alert("Sorry, it doesn't match");
-	}
-} 
-
+flipCard(0);
+flipCard(2);
